a978fea13911ba7701514f09f78125ac16af1445
Markpagso03 <pagsolinganmark04@gmail.com>
2024-10-15 00:41:08 +0800

    add structure

--- kape_thana_web/kape_thana_web/settings.py
+++ kape_thana_web/kape_thana_web/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'kape_thana_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'kape_thana_web/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
