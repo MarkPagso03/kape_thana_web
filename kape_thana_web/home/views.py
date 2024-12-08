bb388767005a142ad4d25ca427036304442a40e4
Markpagso03 <pagsolinganmark04@gmail.com>
2024-12-08 23:27:25 +0800

    contact us

--- kape_thana_web/home/views.py
+++ kape_thana_web/home/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "home/index.html", {'timestamp': time.time()})
+
+def contact_us(request):
+    return render(request, "contact_us/contact_us.html", {'timestamp': time.time()})
