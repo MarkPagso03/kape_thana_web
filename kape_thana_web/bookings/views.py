3a43d13343a887f0c6c8d8fecd7805f57108b653
Markpagso03 <pagsolinganmark04@gmail.com>
2025-01-10 05:12:17 +0800

    booking

--- kape_thana_web/bookings/views.py
+++ kape_thana_web/bookings/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
+
 from .forms import BookingForm
+from django.contrib import messages
 import time
 
 
@@ -9,7 +12,9 @@ def booking(request):
         form = BookingForm(request.POST)
         if form.is_valid():
             form.save()
-            return render(request, 'home/index.html', {'success': 'success'})
+
+            fb_username = form.cleaned_data.get('fb_username')
+            return render(request, 'home/index.html', {'success': 'success', 'fb_username': fb_username})
         else:
             return render(request, 'booking/booking.html', {'form': form, 'errors': form.errors})
     else:
