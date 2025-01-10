a263f6e386bf7c3bdacbfe718547ef9aea79fa09
Markpagso03 <pagsolinganmark04@gmail.com>
2025-01-10 16:22:38 +0800

    booking

--- kape_thana_web/home/urls.py
+++ kape_thana_web/home/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('contact_us', views.contact_us, name="contact_us"),
-    path('about_us', views.about_us, name="about_us")
-    ]
+    path('about_us', views.about_us, name="about_us"),
+    path('accomodation', views.accomodation, name="accomodation"),
+    path('news', views.news, name="news"),
+]
--- kape_thana_web/home/views.py
+++ kape_thana_web/home/views.py
@@ -10,3 +10,9 @@ def contact_us(request):
 
 def about_us(request):
     return render(request, "about_us/about_us.html", {'timestamp': time.time()})
+
+def accomodation(request):
+    return render(request, "accomodation/accomodation.html", {'timestamp': time.time()})
+
+def news(request):
+    return render(request, "news/news.html", {'timestamp': time.time()})
